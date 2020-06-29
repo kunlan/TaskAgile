f07b6dc5f5dbdd8e0ccb9de8d309799135d230fd
kunlan <273072191@qq.com>
2020-06-29 16:51:06 +0800

    create front-end scaffold

---
+++ front-end/nightwatch.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    selenium: {
+      check_process_delay: 5000
+    },
+    test_settings: {
+      chrome: {
+        desiredCapabilities: {
+          browserName: 'chrome',
+          javascriptEnabled: true,
+          acceptSslCerts: true,
+          chromeOptions: {
+            args: ['--headless', '--no-sandbox', '--disable-gpu'],
+            w3c: false
+          }
+        }
+      }
+    }
+  };
